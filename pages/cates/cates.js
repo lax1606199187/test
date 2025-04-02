7fdad84d9c4d5bf068138d57c3a1c092a081fb31
wangyue <18514219729@163.com>
2025-04-02 13:51:41 +0800

    .

--- pages/cates/cates.js
+++ pages/cates/cates.js
@@ -14,24 +14,83 @@ Page({
     school:'卢傲璇',
     ischecked:false,
     num:26,
+    list:[1,2,3],
+    lists:[{
+      id:1,name:"LAX"
+    }],
     obj:{
       name:"lax"
     },
     userInfo:{
-
+      name:'lax',
+      age:'26',
+      test:123,
     },
 
   },
+    //更新数组
+    updateList(){
+      //新增数组元素
+        //方法一
+        // this.data.list.push(4)
+        // this.setData({
+        //   list:this.data.list
+        // })
+
+        //方法二
+        // const newList=this.data.list.concat(4)
+        // this.setData({
+        //   list:newList
+        // })
+
+
+      //修改数组元素
+      this.setData({
+        'list[1]':6,
+        'lists[0].name':"lax"
+      })
+
+
+      //删除数组元素
+    },
 
     //更新UserInfo
     updateUserInfo(){
       //新增单个/多个属性
-      this.setData({
-        //如果是给对象新增属性，可以将key写成数据路径的方式a.b.c
-        'userInfo.name':'lax',
-        'userInfo.age':'26'
-      })
-  },
+      // this.setData({
+      //   //如果是给对象新增属性，可以将key写成数据路径的方式a.b.c
+      //   'userInfo.name':'lax',
+      //   'userInfo.age':'26'
+      // })
+
+      //方法一、通过对象的形式进行修改数据 
+      // const userInfo={
+      //   ...this.data.userInfo,
+      //   name:'Lax',
+      //   age:26
+      // }
+
+      // this.setData({
+      //   userInfo:userInfo
+      // })
+      //方法二、通过obje。assign对象的形式进行修改数据 
+      // const userInfo =Object.assign(this.data.userInfo,{name:'LAX'},{age:25})
+      // this.setData({
+      //   userInfo:userInfo
+      // })
+
+      //删除单个属性
+      // delete this.data.userInfo.age
+      // this.setData({
+      //   userInfo:this.data.userInfo
+      // })
+
+      //删除多个属性
+      // const{age,test,...rest}=this.data.userInfo
+      // this.setData({
+      //   userInfo:rest
+      // })
+    },
   
   updateNum(){
     //获取数据
