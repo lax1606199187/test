5bcf1f5d1005ddcf6be70d0c4be324ee33d85136
lax-home <1606199187@qq.com>
2025-04-04 23:29:17 +0800

    .

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-// app.js
-App({})
+// // app.js
+// App({
+
+//   /**
+//    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+//    */
+//   onLaunch: function () {
+//     //当进行冷启动时，才会触发onLaunch钩子函数
+//     //如果是热启动，不会触发onLaunch钩子函数，会出发onShow钩子函数
+//     console.log('onLaunch小程序初始化完成时')
+//   },
+
+//   /**
+//    * 当小程序启动，或从后台进入前台显示，会触发 onShow
+//    */
+//   onShow: function (options) {
+//     console.log('onShow小程序初始化完成时')
+//   },
+
+//   /**
+//    * 当小程序从前台进入后台，会触发 onHide
+//    */
+//   onHide: function () {
+//     console.log('onHide小程序初始化完成时')
+//   },
+
+//   /**
+//    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+//    */
+//   onError: function (msg) {
+    
+//   }
+// })
+
--- pages/test3/test3.js
+++ pages/test3/test3.js
@@ -1,18 +1,42 @@
-// pages/test2/test2.js
+// pages/test3/test3.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    num:5,
-    isFlag:true
+    a:""
   },
-  //更新num
-  updateNum(){
-    this.setData({
-      num:this.data.num+1
+
+  //获取数据
+  getData(){
+    //如果需要发起网络请求，需要使用wx.request API
+    wx.request({
+      //接口地址
+       url: 'http://localhost:8001/lax',
+      //请求方式
+      method:'GET',
+      //请求参数
+      data:{},
+      //请求头
+      header:{},
+      //API调用成功以后，执行的回调
+      success:(res)=>{
+        console.log(res)
+        this.setData({
+          a:res.data.message,
+        })
+      },
+      //API调用失败以后，执行的回调
+      fail:(err)=>{
+        console.log(err)
+      },
+      //API不管调用成功还是失败以后，执行的回调
+      complete:(res)=>{
+        console.log(res)
+      },
     })
+
   },
   /**
    * 生命周期函数--监听页面加载
